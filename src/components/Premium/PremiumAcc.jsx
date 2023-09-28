a4fdc4a2a1a5a9775ab5d31f75029a9a4363e8df
Alihankudyrov <alihankudyrov@gmail.com>
2023-09-28 11:57:08 +0600

    f

--- src/components/Premium/PremiumAcc.jsx
+++ src/components/Premium/PremiumAcc.jsx
@@ -3,7 +3,7 @@ import elipse from '../../img/Ellipse1.svg';
 import elipse2 from '../../img/Ellipse2.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPremiumStatus } from '../../store/account/AccountSlice';
-import styles from './PremiumStyles/Premium.module.css';
+import './PremiumStyles/Premium.css';
 
 const PremiumAcc = () => {
     const premiumStatus = useSelector((state) => state.account.premiumStatus);
@@ -14,29 +14,29 @@ const PremiumAcc = () => {
     };
 
     return (
-        <div className={styles['premium-acc']}>
-            <img className={styles['elipse2']} src={elipse2} alt="" />
-            <img className={styles['elipse']} src={elipse} alt="" />
-            <div className={styles['credit-card-wrapper']}>
-                <div className={`${styles['credit-card']} ${styles['front']}`}>
-                    <div className={styles['card-title']}>CREDIT CARD</div>
-                    <div className={styles['card-number']}>
-                        <div className={styles['card-number-label']}>CARD NUMBER</div>
-                        <div className={styles['card-numbers']}>
-                            <input type="text" className={styles['tbx-number-0']} maxLength="4" />
-                            <input type="text" className={styles['tbx-number-1']} maxLength="4" />
-                            <input type="text" className={styles['tbx-number-2']} maxLength="4" />
-                            <input type="text" className={styles['tbx-number-3']} maxLength="4" />
+        <div className='premium-acc'>
+            <img className='elipse2' src={elipse2} alt="" />
+            <img className='elipse' src={elipse} alt="" />
+            <div className='credit-card-wrapper'>
+                <div className='credit-card front'>
+                    <div className='card-title'>CREDIT CARD</div>
+                    <div className='card-number'>
+                        <div className='card-number-label'>CARD NUMBER</div>
+                        <div className='card-numbers'>
+                            <input type="text" className='tbx-number-0' maxLength="4" />
+                            <input type="text" className='tbx-number-1' maxLength="4" />
+                            <input type="text" className='tbx-number-2' maxLength="4" />
+                            <input type="text" className='tbx-number-3' maxLength="4" />
                         </div>
                     </div>
-                    <div className={styles['card-expiration']}>
-                        <div className={styles['card-expiration-label']}>
+                    <div className='card-expiration'>
+                        <div className='card-expiration-label'>
                             <div>VALID</div>
                             <div>THRU</div>
                         </div>
-                        <div className={styles['card-expiration-month']}>
-                            <div className={styles['card-expiration-month-label']}>MONTH</div>
-                            <select className={styles['drp-expiration-month']}>
+                        <div className='card-expiration-month'>
+                            <div className='card-expiration-month-label'>MONTH</div>
+                            <select className='drp-expiration-month'>
                                 <option value="01">01</option>
                                 <option value="02">03</option>
                                 <option value="03">03</option>
@@ -51,10 +51,10 @@ const PremiumAcc = () => {
                                 <option value="12">12</option>
                             </select> 
                         </div>
-                        <div className={styles['card-expiration-slash']}>/</div>
-                        <div className={styles['card-expiration-year']}>
-                            <div className={styles['card-expiration-year-label']}>YEAR</div>
-                            <select className={styles['drp-expiration-year']}>
+                        <div className='card-expiration-slash'>/</div>
+                        <div className='card-expiration-year'>
+                            <div className='card-expiration-year-label'>YEAR</div>
+                            <select className='drp-expiration-year'>
                                 <option value="22">22</option>
                                 <option value="23">23</option>
                                 <option value="24">24</option>
@@ -66,19 +66,19 @@ const PremiumAcc = () => {
                         </div>
                     </div>
                 </div>
-                <div className={`${styles['credit-card']} ${styles['back']} ${styles['unfocused']}`}>
-                    <div className={styles['magnetic-strip']}></div>
-                    <div className={styles['credit-card-ccv']}>
-                        <div className={styles['credit-card-ccv-label']}>CCV CODE</div>
-                        <div className={styles['credit-card-ccv-strip']}>
-                            <div className={styles['credit-card-ccv-box']}></div>
-                            <input type="text" className={styles['tbx-ccv']} maxLength="4" />
-                            <div className={styles['credit-card-ccv-prefix']}>XXXX</div>
+                <div className='credit-card back unfocused'>
+                    <div className='magnetic-strip'></div>
+                    <div className='credit-card-ccv'>
+                        <div className='credit-card-ccv-label'>CCV CODE</div>
+                        <div className='credit-card-ccv-strip'>
+                            <div className='credit-card-ccv-box'></div>
+                            <input type="text" className='tbx-ccv' maxLength="4" />
+                            <div className='credit-card-ccv-prefix'>XXXX</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <button onClick={handleUpgradeToPremium}>order</button>
+            <button className='btn' onClick={handleUpgradeToPremium}>order</button>
         </div>
     );
 };
